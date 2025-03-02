cc6fc3781d7cca17c7bee71b20c34727e3d36c92
Michael Leuchtenburg <michael@slashhome.org>
2025-03-02 09:54:37 -0500

    Repro Saghen/blink.cmp#636

---
+++ README.md
@@ -0,0 +1,11 @@
+Repro of https://github.com/Saghen/blink.cmp/issues/636
+
+To run:
+`nvim --clean -u clipboard.lua file.txt`
+
+Then, in nvim:
+`Vyi<esc>i`
+or
+`i<esc>Vyi`
+
+You should see "Waiting for OSC 52 response from the terminal. Press Ctrl-C to interrupt..."
---
+++ clipboard.lua
@@ -0,0 +1,14 @@
+-- Clipboard configuration
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+vim.opt.clipboard = "unnamedplus" -- use the '+' register by default
+require 'blink-cmp'.setup()
