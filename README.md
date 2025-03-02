4cf2e7cdfa6dbe43898bf928e07199ba0dee111a
Michael Leuchtenburg <michael@slashhome.org>
2025-03-02 11:13:46 -0500

    Fully minimized repro for upstream

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ For the same bug without blink:
 `nvim --clean -u without-blink.lua file.txt`
 
 and the same behavior occurs.
+
+For the most minimized version, you can run it with e.g. 
+nix run github:nix-community/neovim-nightly-overlay#neovim -- --clean -u minimized.lua file.txt
+
+Any command to enter insert mode will then stall trying to fetch the register.
---
+++ minimized.lua
@@ -0,0 +1,18 @@
+-- Clipboard configuration
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  callback = function()
+    vim.fn.getreg('+')
+  end,
+})
